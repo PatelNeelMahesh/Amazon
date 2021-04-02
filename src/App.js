d6ac40651d6038511d32a1bf6a3bfd5f683de52b
Neel <pneel361@gmail.com>
2021-04-02 18:11:29 +0530

    commit on 'master'

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Time to build amazon. 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          But Let's Learn React First.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
